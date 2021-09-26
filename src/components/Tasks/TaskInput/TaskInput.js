f5b6f5e5d492e5323adaf1e8f5b80dc0367b5d3f
krupa2029 <kjkotia10@gmail.com>
2021-09-26 14:07:39 +0530

    Initial Commit: Simple React UI Feat: UserInput, Add-Task, Delete-Task

---
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './TaskInput.css';
+
+const TaskInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const goalInputChangeHandler = event => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>Task Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Task</Button>
+    </form>
+  );
+};
+
+export default TaskInput;
