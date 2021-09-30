c5260b3eccb0c8646e15fb7fb0c96930c8c383e0
krupa2029 <kjkotia10@gmail.com>
2021-10-01 02:15:31 +0530

    Added Styled Components in TaskInput Comp. Also worked with Dynamic props in that.

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './TaskInput.css';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+  color: ${props => (props.invalid ? 'red' : 'black')};
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+  background: ${props => props.invalid ? 'rgb(228, 191, 191)': 'transparent'};
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+
+`;
 
 const TaskInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -25,13 +54,10 @@ const TaskInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl invalid={!isValid}>
         <label>Task Goal</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
-      </div>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add Task</Button>
     </form>
   );
